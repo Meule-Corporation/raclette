93ff3148a97def2f164dc9a1a067c8de0751f4a0
hy0ug0 <hugo.contreras.pro@gmail.com>
2021-10-01 14:25:19 +0200

    Refactor countdown component

---
+++ components/countdown.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-row v-if="diff > 0">
+    <v-col align="center">
+      <v-chip dark color="teal" class="ma-2" x-large>
+        {{ twoDigits(days) }} {{ $tc('day', days) }}
+      </v-chip>
+      <v-chip dark color="teal" class="ma-2" x-large>
+        {{ twoDigits(hours) }} {{ $tc('hour', hours) }}
+      </v-chip>
+      <v-chip dark color="teal" class="ma-2" x-large>
+        {{ twoDigits(minutes) }} {{ $t('minute') }}
+      </v-chip>
+      <v-chip dark color="teal" class="ma-2" x-large>
+        {{ twoDigits(seconds) }} {{ $t('second') }}
+      </v-chip>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+const DEADLINE = 'October 12, 2021'
+let interval = null
+
+export default {
+  data() {
+    return {
+      now: Math.trunc(new Date().getTime() / 1000),
+      date: null,
+      diff: 0,
+    }
+  },
+  computed: {
+    seconds() {
+      return Math.trunc(this.diff) % 60
+    },
+    minutes() {
+      return Math.trunc(this.diff / 60) % 60
+    },
+    hours() {
+      return Math.trunc(this.diff / 60 / 60) % 24
+    },
+    days() {
+      return Math.trunc(this.diff / 60 / 60 / 24)
+    },
+  },
+  watch: {
+    now() {
+      this.diff = this.date - this.now
+      if (this.diff <= 0) {
+        this.diff = 0
+        // Remove interval
+        clearInterval(interval)
+      }
+    },
+  },
+  created() {
+    const endTime = DEADLINE
+    this.date = Math.trunc(Date.parse(endTime.replace(/-/g, '/')) / 1000)
+    if (!this.date) {
+      throw new Error("Invalid props value, correct the 'deadline' or 'end'")
+    }
+    interval = setInterval(() => {
+      this.now = Math.trunc(new Date().getTime() / 1000)
+    }, 1000)
+  },
+  destroyed() {
+    clearInterval(interval)
+  },
+  methods: {
+    twoDigits(value) {
+      if (value.toString().length <= 1) {
+        return '0' + value.toString()
+      }
+      return value.toString()
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,82 +2,15 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <h1 class="text-h1 text-center mb-12">{{ $t('raclette') }}</h1>
-      <v-row>
-        <v-col align="center">
-          <v-chip dark color="teal" class="ma-2" x-large>
-            {{ twoDigits(days) }} {{ days > 1 ? 'days' : 'day' }}
-          </v-chip>
-          <v-chip dark color="teal" class="ma-2" x-large>
-            {{ twoDigits(hours) }} {{ hours > 1 ? 'hours' : 'hour' }}
-          </v-chip>
-          <v-chip dark color="teal" class="ma-2" x-large>
-            {{ twoDigits(minutes) }} min
-          </v-chip>
-          <v-chip dark color="teal" class="ma-2" x-large>
-            {{ twoDigits(seconds) }} sec
-          </v-chip>
-        </v-col>
-      </v-row>
+      <countdown />
     </v-col>
   </v-row>
 </template>
 
 <script>
-const DEADLINE = 'October 12, 2021'
-let interval = null
+import countdown from '@/components/countdown'
 
 export default {
-  data() {
-    return {
-      now: Math.trunc(new Date().getTime() / 1000),
-      date: null,
-      diff: 0,
-    }
-  },
-  computed: {
-    seconds() {
-      return Math.trunc(this.diff) % 60
-    },
-    minutes() {
-      return Math.trunc(this.diff / 60) % 60
-    },
-    hours() {
-      return Math.trunc(this.diff / 60 / 60) % 24
-    },
-    days() {
-      return Math.trunc(this.diff / 60 / 60 / 24)
-    },
-  },
-  watch: {
-    now() {
-      this.diff = this.date - this.now
-      if (this.diff <= 0) {
-        this.diff = 0
-        // Remove interval
-        clearInterval(interval)
-      }
-    },
-  },
-  created() {
-    const endTime = DEADLINE
-    this.date = Math.trunc(Date.parse(endTime.replace(/-/g, '/')) / 1000)
-    if (!this.date) {
-      throw new Error("Invalid props value, correct the 'deadline' or 'end'")
-    }
-    interval = setInterval(() => {
-      this.now = Math.trunc(new Date().getTime() / 1000)
-    }, 1000)
-  },
-  destroyed() {
-    clearInterval(interval)
-  },
-  methods: {
-    twoDigits(value) {
-      if (value.toString().length <= 1) {
-        return '0' + value.toString()
-      }
-      return value.toString()
-    },
-  },
+  components: { countdown },
 }
 </script>
