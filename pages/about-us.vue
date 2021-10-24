41d7cfaa600798aba8deaac7cf5620e5b7a15a13
Clément Gibaud <git@gibaud.info>
2021-10-24 20:40:43 +0200

    Adds about-us page

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,38 +1,69 @@
 <template>
   <v-container>
+  <h2 class="text-sm-h2 mb-6 text-center">Les co-fondateurs</h2>
   <v-row justify="center" align="center">
     <v-col cols="12">
-      <cheeseTitle></cheeseTitle>
     </v-col>
-    <v-col cols="12">
+    <v-col cols="9">
       <card-cheese>
         <v-img
             class="white--text align-end"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            src="/img/raclette-world-co-funders.jpg"
         >
-          <v-card-title>Top 10 Australian beaches</v-card-title>
+          <v-card-title>Un trio pour l'intelligence artificielle au service de la raclette</v-card-title>
         </v-img>
         <v-card-text class="text--primary">
-          <div>Whitehaven Beach</div>
-
-          <div>Whitsunday Island, Whitsunday Islands</div>
+          <div>Ce pari fou que se sont lancés les 3 co-fondateurs de Raclette.World : mettre l'intelligence artificielle au service de la raclette.</div>
+          <div>Fruit de centaines d'heures de recherche et développement, les trois co-fondateurs ont mis au point une technologie d'IA post-quantique basée sur la blockchain dans le metaverse. Cette technologie enviée par de nombreuses entreprises de la Silicon Valley rends enfin accessible au plus grand nombre un calculateur de raclette d'une efficacité sans pareil.</div>
         </v-card-text>
       </card-cheese>
     </v-col>
   </v-row>
+  <h2 class="text-sm-h2 mb-6 text-center">Nos valeurs</h2>
   <v-row>
     <v-col cols="12" sm="4">
       <card-cheese>
         <v-img
             class="white--text align-end"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            src="/img/1.jpg"
         >
-          <v-card-title>Top 10 Australian beaches</v-card-title>
+          <v-card-title>L'exigence</v-card-title>
+        </v-img>
+      </card-cheese>
+    </v-col>
+    <v-col cols="12" sm="4">
+      <card-cheese>
+        <v-img
+            class="white--text align-end"
+            src="/img/3.jpg"
+        >
+          <v-card-title>La qualité</v-card-title>
+        </v-img>
+      </card-cheese>
+    </v-col>
+    <v-col cols="12" sm="4" >
+      <card-cheese>
+        <v-img
+            class="white--text align-end"
+            src="/img/2.jpg"
+        >
+          <v-card-title>La persévérance</v-card-title>
+        </v-img>
+      </card-cheese>
+    </v-col>
+  </v-row>
+  <h2 class="text-sm-h2 mb-6 text-center">Ils utilisent Raclette.World</h2>
+  <v-row>
+    <v-col cols="12" sm="4">
+      <card-cheese>
+        <v-img
+            class="white--text align-end"
+            src="/img/4.jpg"
+        >
+          <v-card-title>François Hollande</v-card-title>
         </v-img>
         <v-card-text class="text--primary">
-          <div>Whitehaven Beach</div>
-
-          <div>Whitsunday Island, Whitsunday Islands</div>
+          <div><i>« Cet outil est vraiment pratique, même pour les sans-dents. Niehéééé. »</i></div>
         </v-card-text>
       </card-cheese>
     </v-col>
@@ -40,14 +71,12 @@
       <card-cheese>
         <v-img
             class="white--text align-end"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            src="/img/5.jpg"
         >
-          <v-card-title>Top 10 Australian beaches</v-card-title>
+          <v-card-title>Des gens random</v-card-title>
         </v-img>
         <v-card-text class="text--primary">
-          <div>Whitehaven Beach</div>
-
-          <div>Whitsunday Island, Whitsunday Islands</div>
+          <div><i>« Sous prétexte que nous sommes des gens random de Getty Images, on aurait pas le droit d'utiliser Raclette.World ? »</i></div>
         </v-card-text>
       </card-cheese>
     </v-col>
@@ -55,18 +84,16 @@
       <card-cheese>
         <v-img
             class="white--text align-end"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            src="/img/6.jpg"
         >
-          <v-card-title>Top 10 Australian beaches</v-card-title>
+          <v-card-title>Jul</v-card-title>
+        </v-card-text>
         </v-img>
         <v-card-text class="text--primary">
-          <div>Whitehaven Beach</div>
-
-          <div>Whitsunday Island, Whitsunday Islands</div>
+          <div><i>« On m'appel l'OVNI car j'achète toujours pile poile ce qu'il faut pour les raclettes »</i></div>
         </v-card-text>
       </card-cheese>
     </v-col>
-
   </v-row>
   </v-container>
 </template>
